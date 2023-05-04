a3eb47a76d1efeb860b2c38ebf298866b93c2da2
Heechan <ckstmznf@naver.com>
2023-05-04 15:35:27 +0900

    한국어 -> 일본어 -> 영어 번역 기능

---
+++ app/src/main/java/com/heechan/moredetailed/Language.kt
@@ -0,0 +1,10 @@
+package com.heechan.moredetailed
+
+enum class Language(
+    val langCode : String,
+    val nameKo : String
+) {
+    KO("ko", "한국어"),
+    EN("en", "영어"),
+    JP("ja", "일본어"),
+}
--- app/src/main/java/com/heechan/moredetailed/MainViewModel.kt
+++ app/src/main/java/com/heechan/moredetailed/MainViewModel.kt
@@ -4,37 +4,51 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.heechan.moredetailed.model.data.PapagoRes
 import com.heechan.moredetailed.model.service.PapagoService
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     private val papagoService = RetrofitClient.get().create(PapagoService::class.java)
 
     val inputMessage = MutableLiveData<String>()
-    val startLanage = MutableLiveData<String>()
-    val resultData = MutableLiveData<PapagoRes>()
+    val resultMessage = MutableLiveData<String>()
+    val startLang = MutableLiveData<String>()
 
     fun translated() {
-        if(inputMessage.value.isNullOrBlank()){
+        if (inputMessage.value.isNullOrBlank()) {
             return
         }
 
-        viewModelScope.launch {
-            val result = papagoService.translate(
-                sourceLang = "ko",
-                targetLang = "en",
-                text = inputMessage.value.toString()
-            )
-
-            val body = result.body() ?: return@launch
-            resultData.value = body
-
-            if (result.isSuccessful) {
-                val translatedText = body.message.result.translatedText
-                Log.d("[ApiTest]", translatedText)
+        viewModelScope.launch(
+            CoroutineExceptionHandler { _, e ->
+                Log.e("[Translated]", e.toString())
             }
+        ) {
+            val input2jp = translated(inputMessage.value!!, Language.KO, Language.JP)
+            val jp2target = translated(input2jp, Language.JP, Language.EN)
+
+            resultMessage.value = jp2target
+        }
+    }
+
+    private suspend fun translated(
+        message: String,
+        startLang: Language,
+        targetLang: Language
+    ): String {
+        val response = papagoService.translate(
+            sourceLang = startLang.langCode,
+            targetLang = targetLang.langCode,
+            text = message
+        )
+
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            return body.message.result.translatedText
         }
 
+        Log.e("[Translated]", response.errorBody().toString())
+        throw Exception("Papago api exception")
     }
 }
